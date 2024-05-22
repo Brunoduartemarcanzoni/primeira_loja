ca301c5acb6c7596b54110c2a7fdfaf4789863cc
bruno duarte <159266616+Brunoduartemarcanzoni@users.noreply.github.com>
2024-05-22 17:24:34 -0300

    22/05

---
+++ script.js
@@ -0,0 +1,11 @@
+function logar(){
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin" && senha == "admin"){
+        alert('bem vindo');
+        location.href = index.html;
+    }else{
+        alert('usuario ou senha incorreto');
+    }
+}
